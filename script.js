c35ec4bc67c30d1dbe3f74113df74e462c3d0633
Александр Чернобров <alesandro83@mail.ru>
2024-05-15 15:29:59 +0300

    Расчеты

--- script.js
+++ script.js
@@ -23,3 +23,30 @@ console.log(unique);
 // }
 
 // console.log(getNumber(2, 3));
+
+// dice
+function simulatingDice(dice) {
+  let diceArr = dice.split('');
+  diceArr.shift();
+  let diceStr = diceArr.join('');
+  
+  if (Number(diceStr) % 2 !== 0) {
+  return 'Число должно быть чётным'
+  }
+  
+  if (Number(diceStr) < 4) {
+  return 'Число не поддерживается. Должно быть больше'
+  }
+  
+  if (Number(diceStr) > 20) {
+  return 'Число не поддерживается. Должно быть меньше'
+  }
+  
+  if (Number(diceStr) === 14 || Number(diceStr) === 18) {
+  return 'Число должно быть больше или меньше'
+  }
+  
+  return Math.floor(Math.random() * Number(diceStr) + 1);
+  }
+  
+  console.log(simulatingDice('D16'));
